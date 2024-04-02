54bbcbeae621d2560ba6b1841caf4dd955bf1e43
JayOHx <119745695+Jay0Hx@users.noreply.github.com>
2024-04-02 10:05:12 +0100

    Update PredatorV1.lua

--- PredatorV1.lua
+++ PredatorV1.lua
@@ -12,7 +12,6 @@ local predator_setting = {
 
 function script.windowMain(PredatorV1)
   ui.tabBar("main_tabs", function()
-
     ui.tabItem("Settings", function()
       if ui.checkbox("Enable 'Drivers' tab.", predator_setting.menu_settings.drivers_tab) then  
         predator_setting.menu_settings.drivers_tab = not predator_setting.menu_settings.drivers_tab   
@@ -21,16 +20,38 @@ function script.windowMain(PredatorV1)
         predator_setting.menu_settings.client_information = not predator_setting.menu_settings.client_information   
       end
     end)
-
     if predator_setting.menu_settings.drivers_tab then
       ui.tabItem("Drivers", function()
       end)
     end
 
     if predator_setting.menu_settings.client_information then
-      ui.tabItem("Client Information", function()
+      ui.tabItem("🧑 Your Information", function()
+        ui.treeNode("Client Information", ui.TreeNodeFlags.DefaultClosed and ui.TreeNodeFlags.Framed, function ()
+          ui.text("   🎌 • Race Position: "..ac.getCar().racePosition)
+          ui.text("   👋 • Display Name: "..ac.getDriverName())
+          ui.text("   🩹 • Patch Version: "..ac.getPatchVersion())
+          ui.text("   📐 • Current FOV: "..ac.getCameraFOV())
+          ui.text("   🗺️ • Position:")
+          ui.sameLine()
+          ui.text(ac.getCameraPosition())
+          ui.text("   📷 • Camera:")
+          ui.sameLine()
+          ui.text(ac.getCameraDirection()) 
+        end)
+        ui.treeNode("Track Information", ui.TreeNodeFlags.DefaultClosed and ui.TreeNodeFlags.Framed, function ()
+          ui.text("   🛣️ • Track ID: "..ac.getTrackId())
+          ui.text("   🛣️ • Track Name: "..ac.getTrackName())
+        end)
+        ui.treeNode("Car Information", ui.TreeNodeFlags.DefaultClosed and ui.TreeNodeFlags.Framed, function ()
+          ui.text("   🚩 • Car Country: "..ac.getCarCountry())
+          ui.text("   🚗 • Car Name: "..ac.getCarName())
+          ui.text("   🚗 • Car Brand: "..ac.getCarBrand())
+          ui.text("   🪪 • Car ID: "..ac.getCarID())
+          ui.text("   💨 • Current Speed: "..ac.getCarSpeedKmh())
+          ui.text("   🛞 • Tyres Name: "..ac.getTyresName())  
+        end)
       end)
     end
-
   end) 
 end
